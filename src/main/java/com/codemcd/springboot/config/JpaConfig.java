2130bf7b0e5efa581c71c467d29ae014d72fbd9c
CODEMCD <psbum1609@gmail.com>
2020-02-21 14:39:17 +0900

    Chapter 05 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현
    * 스프링 시큐리티 + OAuth 2.0으로 구글, 네이버 로그인 연동
    
    * 세션 저장소를 데이터베이스로 사용
    
    * User 정보 ArgumentResolver 적용

---
+++ src/main/java/com/codemcd/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.codemcd.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
